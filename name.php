21398e5129044cdebf1b848a723886e49ea12583
Dragneels <nanimogozaru@gmail.com>
2015-09-04 20:08:10 +0700

    Tugas Sistem Informasi Terdistribusi

---
+++ name.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Nama          : Arif Sholehuddin";
+	echo "NIM           : 13/350955/SV/04024";
+	echo "Alamat Github : https://github.com/Dragneels";
+	echo "Repository    : https://github.com/Dragneels/FirstCommit";
+?>
